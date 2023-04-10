389b06aa437eed6ebb28b060cf65813db3ac3791
duartemelo <43653646+duartemelo@users.noreply.github.com>
2023-04-10 16:13:37 +0100

    Lesson 33

--- flutter_complete_guide/lib/main.dart
+++ flutter_complete_guide/lib/main.dart
@@ -1,13 +1,48 @@
 import 'package:flutter/material.dart';
 
+// void main () {
+//   runApp(MyApp());
+// }
 
-void main () {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Answer chosen!');
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hello!'),);
+    var questions = [
+      'What\s your favourite color?',
+      'What\s your favourite animal?'
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: [
+            Text('The question!'),
+            ElevatedButton(
+              onPressed: answerQuestion,
+              child: Text('Answer 1'),
+            ),
+            ElevatedButton(
+              onPressed: () => print('Answer 2 chosen!'),
+              child: Text('Answer 2'),
+            ),
+            ElevatedButton(
+              onPressed: () { // sem seta!
+                // ...
+                print('Answer 3 chosen!');
+              },
+              child: Text('Answer 3'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
