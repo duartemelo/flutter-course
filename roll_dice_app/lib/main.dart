0353f630bb1180138c3273a320ce9f0e20aec609
duartemelo <43653646+duartemelo@users.noreply.github.com>
2023-04-11 21:39:45 +0100

    Lesson 31 new course

--- roll_dice_app/lib/main.dart
+++ roll_dice_app/lib/main.dart
@@ -2,10 +2,22 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World'),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 28, 6, 66),
+                Color.fromARGB(255, 56, 10, 135),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const Center(
+            child: Text('Hello World'),
+          ),
         ),
       ),
     ),
