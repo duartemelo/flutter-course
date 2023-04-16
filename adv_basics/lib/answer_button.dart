df12e32f54de099fa8f8ee1ae1f45691966dcd33
duartemelo <43653646+duartemelo@users.noreply.github.com>
2023-04-16 20:02:37 +0100

    Lesson 68

---
+++ adv_basics/lib/answer_button.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class AnswerButton extends StatelessWidget {
+  const AnswerButton({
+    required this.answerText,
+    required this.onClick,
+    super.key,
+  });
+
+  final String answerText;
+  final void Function() onClick;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onClick,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: const Color.fromARGB(255, 33, 1, 95),
+        foregroundColor: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(40),
+        ),
+        padding: const EdgeInsets.symmetric(
+          horizontal: 40,
+          vertical: 10,
+        ),
+      ),
+      child: Text(answerText),
+    );
+  }
+}
--- adv_basics/lib/questions_screen.dart
+++ adv_basics/lib/questions_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:adv_basics/answer_button.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -7,14 +8,39 @@ class QuestionsScreen extends StatefulWidget {
   State<QuestionsScreen> createState() {
     return _QuestionsScreenState();
   }
-
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
-  
   @override
   Widget build(BuildContext context) {
-    return const Text('QuestionsScreen');
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'The question...',
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          AnswerButton(
+            answerText: 'Answer 1',
+            onClick: () {},
+          ),
+          AnswerButton(
+            answerText: 'Answer 2',
+            onClick: () {},
+          ),
+          AnswerButton(
+            answerText: 'Answer 3',
+            onClick: () {},
+          ),
+        ],
+      ),
+    );
   }
-
 }
