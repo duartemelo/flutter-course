c4a9421ea1032e2496cb916b3beb9a16e3bfeb84
Duarte Melo <duarteribeirodemelo@hotmail.com>
2023-04-21 15:41:48 +0100

    Lesson 120

--- expense_tracker/lib/widgets/expenses_list/expenses_list.dart
+++ expense_tracker/lib/widgets/expenses_list/expenses_list.dart
@@ -6,15 +6,23 @@ class ExpensesList extends StatelessWidget {
   const ExpensesList({
     super.key,
     required this.expenses,
+    required this.onRemove,
   });
 
   final List<Expense> expenses;
+  final void Function(Expense expense) onRemove;
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: expenses.length,
-      itemBuilder: (ctx, index) => ExpenseItem(expenses[index]),
+      itemBuilder: (ctx, index) => Dismissible(
+        key: ValueKey(expenses[index]),
+        onDismissed: (direction) => onRemove(expenses[index]),
+        child: ExpenseItem(
+          expenses[index],
+        ),
+      ),
     );
   }
 }
