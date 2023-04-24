11b794fc8417ebb32e4cc15b52dbd5c61b1f8b40
Duarte Melo <duarteribeirodemelo@hotmail.com>
2023-04-24 15:45:25 +0100

    Lesson 128

--- expense_tracker/lib/widgets/expenses.dart
+++ expense_tracker/lib/widgets/expenses.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/widgets/chart/chart.dart';
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/models/expense.dart';
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
@@ -87,7 +88,9 @@ class _ExpensesState extends State<Expenses> {
       ),
       body: Column(
         children: [
-          const Text('The chart'),
+          Chart(
+            expenses: _registeredExpenses,
+          ),
           Expanded(
             child: mainContent,
           ),
